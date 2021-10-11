a685d20fd10d67bb418ef9a189eabd320aa096d0
Barbara Poon <barbara.poon@algorand.com>
2021-10-11 16:59:10 -0400

    Use c5d.4xlarge machine

--- test/testdata/deployednettemplates/recipes/perftest/gen_topology.py
+++ test/testdata/deployednettemplates/recipes/perftest/gen_topology.py
@@ -1,5 +1,5 @@
 node_types = {"R":1, "N":3, "NPN":20}
-node_size = {"R":"-m5d.4xl", "N":"-m5d.4xl", "NPN":"-m5d.4xl"}
+node_size = {"R":"-c5d.4xl", "N":"-c5d.4xl", "NPN":"-c5d.4xl"}
 regions = [
     "AWS-US-EAST-2"
 ]
